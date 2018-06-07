db777f82d264b268d382ff3f710d9dc6aa0366d6
NUmeroAndDev <numero.dev.and@gmail.com>
2018-06-07 16:29:38 +0900

    init

---
+++ app/src/main/java/com/numero/material_gallery/model/DesignComponent.kt
@@ -0,0 +1,72 @@
+package com.numero.material_gallery.model
+
+import androidx.annotation.StringRes
+import com.numero.material_gallery.R
+
+enum class DesignComponent(@StringRes val nameRes: Int, val isEnable: Boolean) {
+
+    BACKDROP(
+            R.string.backdrop,
+            false
+    ),
+
+    BOTTOM_APP_BAR(
+            R.string.bottom_app_bar,
+            true
+    ),
+
+    BOTTOM_NAVIGATION(
+            R.string.bottom_navigation,
+            true
+    ),
+
+    BOTTOM_SHEET_MODAL(
+            R.string.bottom_sheet_modal,
+            true
+    ),
+
+    BOTTOM_SHEET_PERSISTENT(
+            R.string.bottom_sheet_persistent,
+            true
+    ),
+
+    BUTTON(
+            R.string.button,
+            true
+    ),
+
+    FAB(
+            R.string.fab,
+            true
+    ),
+
+    CARD(
+            R.string.card,
+            true
+    ),
+
+    CHIPS(
+            R.string.backdrop,
+            true
+    ),
+
+    NAVIGATION_DRAWER(
+            R.string.navigation_drawer,
+            true
+    ),
+
+    SNACKBAR(
+            R.string.snachbar,
+            true
+    ),
+
+    TAB(
+            R.string.tab,
+            true
+    ),
+
+    TEXT_FIELDS(
+            R.string.text_field,
+            true
+    )
+}
