b79ba1c5dff139bb4ce74fe2887db7177ed2f303
NUmeroAndDev <numero.dev.and@gmail.com>
2018-11-01 15:17:32 +0900

    CardのCornerを変更できるように

--- app/src/main/java/com/numero/material_gallery/activity/MaterialCardActivity.kt
+++ app/src/main/java/com/numero/material_gallery/activity/MaterialCardActivity.kt
@@ -9,6 +9,7 @@ import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
 import com.numero.material_gallery.R
 import com.numero.material_gallery.fragment.ColorInfoBottomSheetDialog
+import com.numero.material_gallery.model.Corner
 import com.numero.material_gallery.model.Elevation
 import com.numero.material_gallery.repository.IConfigRepository
 import kotlinx.android.synthetic.main.activity_material_card.*
@@ -43,8 +44,20 @@ class MaterialCardActivity : AppCompatActivity() {
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
             }
         })
+        cornerSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                updateCorner(Corner.values()[progress])
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
 
         updateElevation(Elevation.values()[elevationSeekBar.progress])
+        updateCorner(Corner.values()[cornerSeekBar.progress])
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -70,6 +83,12 @@ class MaterialCardActivity : AppCompatActivity() {
         val elevationSize = resources.getDimension(elevation.dimenRes)
         materialCardView.cardElevation = elevationSize
         elevationInfoTextView.text = getString(R.string.elevation_info_format).format(convertPxToDp(elevationSize).toInt())
+    }
+
+    private fun updateCorner(corner: Corner) {
+        val cornerSize = resources.getDimension(corner.dimenRes)
+        materialCardView.radius = cornerSize
+        cornerInfoTextView.text = getString(R.string.corner_info_format).format(convertPxToDp(cornerSize).toInt())
     }
 
     private fun convertPxToDp(px: Float): Float {
