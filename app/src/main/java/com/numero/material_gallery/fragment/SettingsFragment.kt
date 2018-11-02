cca6812e19ed01ad7917b63c8f97a958d4b70ca3
NUmeroAndDev <numero.dev.and@gmail.com>
2018-11-02 12:25:32 +0900

    MaterialComponentsのバージョン表記を追加

--- app/src/main/java/com/numero/material_gallery/fragment/SettingsFragment.kt
+++ app/src/main/java/com/numero/material_gallery/fragment/SettingsFragment.kt
@@ -2,6 +2,7 @@ package com.numero.material_gallery.fragment
 
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
+import com.numero.material_gallery.BuildConfig
 import com.numero.material_gallery.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
@@ -9,7 +10,14 @@ class SettingsFragment : PreferenceFragmentCompat() {
         setPreferencesFromResource(R.xml.settings, rootKey)
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        findPreference(KEY_MATERIAL_VERSION).summary = BuildConfig.MATERIAL_COMPONENTS_VERSION
+    }
+
     companion object {
+        private const val KEY_MATERIAL_VERSION = "material_components_version"
+
         fun newInstance(): SettingsFragment = SettingsFragment()
     }
 }
