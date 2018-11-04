44b926d932ca1d00966811605723f1a8e145df5b
NUmeroAndDev <numero.dev.and@gmail.com>
2018-11-04 10:38:05 +0900

    LiftOnScrollを見やすいように

--- app/src/main/java/com/numero/material_gallery/activity/top_app_bar/LiftOnScrollActivity.kt
+++ app/src/main/java/com/numero/material_gallery/activity/top_app_bar/LiftOnScrollActivity.kt
@@ -2,43 +2,29 @@ package com.numero.material_gallery.activity.top_app_bar
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
-import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.numero.material_gallery.R
-import com.numero.material_gallery.fragment.ColorInfoBottomSheetDialog
-import com.numero.material_gallery.repository.IConfigRepository
+import com.numero.material_gallery.extension.getTintedDrawable
 import kotlinx.android.synthetic.main.activity_lift_on_scroll.*
-import org.koin.android.ext.android.inject
 
 class LiftOnScrollActivity : AppCompatActivity() {
 
-    private val configRepository by inject<IConfigRepository>()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTheme(configRepository.themeRes)
         setContentView(R.layout.activity_lift_on_scroll)
         setSupportActionBar(toolbar)
 
         supportActionBar?.apply {
             setDisplayHomeAsUpEnabled(true)
-            setHomeAsUpIndicator(R.drawable.ic_arrow_back)
+            setHomeAsUpIndicator(getTintedDrawable(R.drawable.ic_arrow_back, Color.BLACK))
         }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_common, menu)
-        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
-            R.id.action_info -> {
-                ColorInfoBottomSheetDialog.newInstance().showIfNeed(supportFragmentManager)
-                true
-            }
             android.R.id.home -> {
                 onBackPressed()
                 true
