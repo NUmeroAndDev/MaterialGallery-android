423763d8b75d79b9d498cb404b3ad06b97145ffa
NUmeroAndDev <numero.dev.and@gmail.com>
2018-12-16 10:43:38 +0900

    ShapeTheming選択の実装

--- app/src/main/java/com/numero/material_gallery/repository/ConfigRepository.kt
+++ app/src/main/java/com/numero/material_gallery/repository/ConfigRepository.kt
@@ -1,6 +1,7 @@
 package com.numero.material_gallery.repository
 
 import android.content.Context
+import android.content.SharedPreferences
 import androidx.preference.PreferenceManager
 import com.numero.material_gallery.R
 
@@ -11,14 +12,43 @@ class ConfigRepository(context: Context) : IConfigRepository {
     override val themeRes: Int
         get() {
             val isDarkTheme = settingsPreference.getBoolean(KEY_IS_DARK_THEME, false)
-            return if (isDarkTheme) {
-                R.style.DarkTheme
-            } else {
-                R.style.DefaultTheme
+            val shapeTheme = settingsPreference.getShapeTheme()
+            return when (shapeTheme) {
+                ShapeTheme.ROUNDED -> {
+                    if (isDarkTheme) {
+                        R.style.DarkTheme_Rounded
+                    } else {
+                        R.style.LightTheme_Rounded
+                    }
+                }
+                ShapeTheme.CUT -> {
+                    if (isDarkTheme) {
+                        R.style.DarkTheme_Cut
+                    } else {
+                        R.style.LightTheme_Cut
+                    }
+                }
             }
         }
 
+    private fun SharedPreferences.getShapeTheme(): ShapeTheme {
+        val value = getString(KEY_SHAPE_THEME, null) ?: return ShapeTheme.ROUNDED
+        return ShapeTheme.findShapeTheme(value)
+    }
+
+    private enum class ShapeTheme(private val value: String) {
+        ROUNDED("rounded"),
+        CUT("cut");
+
+        companion object {
+            fun findShapeTheme(value: String): ShapeTheme {
+                return checkNotNull(values().find { it.value == value })
+            }
+        }
+    }
+
     companion object {
         private const val KEY_IS_DARK_THEME = "is_dark_theme"
+        private const val KEY_SHAPE_THEME = "shape_theme"
     }
 }
