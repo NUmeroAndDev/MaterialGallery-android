94275b2693b3b77d8c5b35215cb47a2d79aaaebc
NUmeroAndDev <numero.dev.and@gmail.com>
2019-07-11 11:18:39 +0900

    Updated material-components

--- app/src/main/java/com/numero/material_gallery/activity/BottomNavigationActivity.kt
+++ app/src/main/java/com/numero/material_gallery/activity/BottomNavigationActivity.kt
@@ -68,11 +68,11 @@ class BottomNavigationActivity : AppCompatActivity(R.layout.activity_bottom_navg
         }
 
         withBadgeBottomNavigation.apply {
-            showBadge(R.id.navigation_item_1)
-            showBadge(R.id.navigation_item_2).apply {
+            getOrCreateBadge(R.id.navigation_item_1)
+            getOrCreateBadge(R.id.navigation_item_2).apply {
                 number = 10
             }
-            showBadge(R.id.navigation_item_3).apply {
+            getOrCreateBadge(R.id.navigation_item_3).apply {
                 number = 1000
             }
         }
