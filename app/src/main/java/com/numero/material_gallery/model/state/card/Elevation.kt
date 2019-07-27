55f9bb207b5f2ff528edd0db0468bb09d53740e8
NUmeroAndDev <numero.dev.and@gmail.com>
2019-07-27 13:27:10 +0900

    Can select card elevation to 24dp

--- app/src/main/java/com/numero/material_gallery/model/state/card/Elevation.kt
+++ app/src/main/java/com/numero/material_gallery/model/state/card/Elevation.kt
@@ -7,5 +7,7 @@ enum class Elevation(@DimenRes val dimenRes: Int) {
     SHADOW_0DP(R.dimen.elevation_0dp),
     SHADOW_2DP(R.dimen.elevation_2dp),
     SHADOW_4DP(R.dimen.elevation_4dp),
-    SHADOW_8DP(R.dimen.elevation_8dp)
+    SHADOW_8DP(R.dimen.elevation_8dp),
+    SHADOW_16DP(R.dimen.elevation_16dp),
+    SHADOW_24DP(R.dimen.elevation_24dp)
 }
