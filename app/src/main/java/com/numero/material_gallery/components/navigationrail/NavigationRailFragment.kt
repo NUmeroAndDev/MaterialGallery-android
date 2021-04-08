9b5397427ce4f6d75daf9ae717034ac1817e17d2
NUmeroAndDev <numero.dev.and@gmail.com>
2021-04-08 18:52:59 +0900

    Added Navigation Rail example

---
+++ app/src/main/java/com/numero/material_gallery/components/navigationrail/NavigationRailFragment.kt
@@ -0,0 +1,66 @@
+package com.numero.material_gallery.components.navigationrail
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.navigationrail.NavigationRailView
+import com.numero.material_gallery.R
+import com.numero.material_gallery.components.MaterialContainerTransformFragment
+import com.numero.material_gallery.databinding.FragmentNavigationRailBinding
+import dev.chrisbanes.insetter.applyInsetter
+
+class NavigationRailFragment :
+    MaterialContainerTransformFragment(R.layout.fragment_navigation_rail) {
+
+    private var _binding: FragmentNavigationRailBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentNavigationRailBinding.bind(view)
+
+        binding.labelVisibilityRadioGroup.setOnCheckedChangeListener { _, checkedId ->
+            binding.navigationRailView.labelVisibilityMode = when (checkedId) {
+                R.id.labelVisibilityAutoRadioButton -> NavigationRailView.LABEL_VISIBILITY_AUTO
+                R.id.labelVisibilitySelectedRadioButton -> NavigationRailView.LABEL_VISIBILITY_SELECTED
+                R.id.labelVisibilityLabeledRadioButton -> NavigationRailView.LABEL_VISIBILITY_LABELED
+                R.id.labelVisibilityUnlabeledRadioButton -> NavigationRailView.LABEL_VISIBILITY_UNLABELED
+                else -> throw Exception("Not defined id")
+            }
+        }
+
+        binding.rootLayout.applyInsetter {
+            type(navigationBars = true) {
+                padding()
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_common, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_current_theme -> {
+                findNavController().navigate(R.id.action_show_ThemeInfoDialog)
+                true
+            }
+            else -> false
+        }
+    }
+}
