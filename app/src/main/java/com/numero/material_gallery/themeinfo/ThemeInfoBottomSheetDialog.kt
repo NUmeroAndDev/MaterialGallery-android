1ca8553d4ecc91c09ecec5f5faa57c720985cf41
NUmeroAndDev <numero.dev.and@gmail.com>
2021-06-24 09:24:48 +0900

    Removed unused code

--- app/src/main/java/com/numero/material_gallery/themeinfo/ThemeInfoBottomSheetDialog.kt
+++ app/src/main/java/com/numero/material_gallery/themeinfo/ThemeInfoBottomSheetDialog.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.FragmentManager
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.numero.material_gallery.R
 import com.numero.material_gallery.databinding.BottomSheetFragmentThemeInfoBinding
@@ -57,22 +56,11 @@ class ThemeInfoBottomSheetDialog : BottomSheetDialogFragment() {
         }
     }
 
-    fun showIfNeeded(fragmentManager: FragmentManager) {
-        if (fragmentManager.findFragmentByTag(TAG) != null) return
-        showNow(fragmentManager, TAG)
-    }
-
     private fun Theme.getToggleButtonId(): Int {
         return when (this) {
             Theme.LIGHT -> R.id.lightThemeButton
             Theme.DARK -> R.id.darkThemeButton
             Theme.SYSTEM_DEFAULT -> R.id.systemThemeButton
         }
-    }
-
-    companion object {
-        private const val TAG = "ThemeInfoBottomSheetDialog"
-
-        fun newInstance(): ThemeInfoBottomSheetDialog = ThemeInfoBottomSheetDialog()
     }
 }
