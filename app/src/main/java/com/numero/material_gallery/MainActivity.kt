b4c97f6737825cdf1a09135a7366ff72c481aca0
NUmeroAndDev <numero.dev.and@gmail.com>
2022-04-14 10:14:13 +0900

    Fixed visibility appbar

--- app/src/main/java/com/numero/material_gallery/MainActivity.kt
+++ app/src/main/java/com/numero/material_gallery/MainActivity.kt
@@ -67,7 +67,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
-        setTheme(R.style.Theme_MaterialGallery_DayNight)
         super.onCreate(savedInstanceState)
         setTheme(configRepository.currentShapeTheme.themeRes)
         if (DynamicColors.isDynamicColorAvailable()) {
